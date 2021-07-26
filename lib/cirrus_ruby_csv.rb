2ec625c5fdf97a853213b24464c527c5eca9acb6
Isaac Sunoo <isaacsunoo@gmail.com>
2021-07-26 11:39:08 -0600

    added date formatter and testing for different date strings:

--- lib/cirrus_ruby_csv.rb
+++ lib/cirrus_ruby_csv.rb
@@ -25,10 +25,13 @@ module CsvFormatter
 
       formatted_hash[key] = if key == 'phone_number'
                               phone_number_formatter(v)
+                            elsif %w(dob effective_date expiry_date).include?(key)
+                              date_formatter(v)
                             else
                               v
                             end
     end
+    formatted_hash
   end
 
   def self.nil_whitespace_cleaner(value)
@@ -41,4 +44,19 @@ module CsvFormatter
     formatted_number = phone_number.gsub(/[^0-9]/, '').split('').last(10).join('').to_s
     '1+'.concat(formatted_number)
   end
+
+  def self.date_formatter(date_string)
+    return 'No Date Provided' if date_string == ''
+    if date_string.include?('/')
+      return Date.strptime(date_string, '%m/%d/%Y').to_s if date_string.split('/').last.length == 4
+      return Date.strptime(date_string, '%m/%d/%y').to_s
+    end
+
+    if date_string.include?('-')
+      return Date.strptime(date_string, '%Y-%m-%d').to_s if date_string.split('-').first.length == 4
+      Date.strptime(date_string, '%m-%d-%y').to_s
+    end
+
+  end
+
 end
--- spec/cirrus_ruby_csv_spec.rb
+++ spec/cirrus_ruby_csv_spec.rb
@@ -18,4 +18,34 @@ RSpec.describe CirrusRubyCsv do
     expect(converted_number).to eq('1+3035551234')
   end
 
+  it "Transform ALL dates to ISO8601 format (YYYY-MM-DD)" do
+    invalid = ''
+    date_1 = '12/12/2010'
+    date_2 = '2/2/1966'
+    date_3 = '6/6/99'
+    date_4 = '1988-02-12'
+    date_5 = '1-11-88'
+
+    expected_1 = '2010-12-12'
+    expected_2 = '1966-02-02'
+    expected_3 = '1999-06-06'
+    expected_4 = '1988-02-12'
+    expected_5 = '1988-01-11'
+    expected_invalid = 'No Date Provided'
+
+    result_1 = CsvFormatter.date_formatter(date_1)
+    result_2 = CsvFormatter.date_formatter(date_2)
+    result_3 = CsvFormatter.date_formatter(date_3)
+    result_4 = CsvFormatter.date_formatter(date_4)
+    result_5 = CsvFormatter.date_formatter(date_5)
+    result_invalid = CsvFormatter.date_formatter(invalid)
+
+    expect(expected_1).to eq(result_1)
+    expect(expected_2).to eq(result_2)
+    expect(expected_3).to eq(result_3)
+    expect(expected_4).to eq(result_4)
+    expect(expected_5).to eq(result_5)
+    expect(expected_invalid).to eq(result_invalid)
+  end
+
 end
