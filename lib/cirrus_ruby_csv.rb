180d84f330ff2cea9bf9242a2cf42d0ca6b572d0
Isaac Sunoo <isaacsunoo@gmail.com>
2021-07-26 09:23:14 -0600

    initial commit

---
+++ lib/cirrus_ruby_csv.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cirrus_ruby_csv/version"
+
+module CirrusRubyCsv
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cirrus_ruby_csv_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CirrusRubyCsv do
+  it "has a version number" do
+    expect(CirrusRubyCsv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
